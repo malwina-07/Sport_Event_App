c3271750efd4eaa3e27b064c2359c53afdb20109
Anna_Rogiewicz <anna.rogiewicz@gmail.com>
2021-05-05 10:09:07 +0200

    Add new files

---
+++ src/main/java/eu/sportEvent/SporteventappApplication.java
@@ -0,0 +1,13 @@
+package eu.sportEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SporteventappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SporteventappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/sportEvent/SporteventappApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.sportEvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SporteventappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
